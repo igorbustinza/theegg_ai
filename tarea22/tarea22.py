2c03819a69ae54bd2efd23e3de7fa6268c93eb82
igorbustinza <ibustinza@gmail.com>
2020-08-17 18:51:40 +0200

    readme tarea22

--- tarea22/tarea22.py
+++ tarea22/tarea22.py
@@ -21,16 +21,16 @@ litrosleche3 = [45,50,34,39,29,40,30,52,31,1]
 
 def  camion_vacas(p,l,C):
 	n=len(p)
-	ant =[[0 ,[]]  for m in  range(C+1)]
+	pre =[[0 ,[]]  for m in  range(C+1)]
 	for i in  range(1,n+1):
-		act =[[0 ,[0  for j in  range(i)]]]
+		pos =[[0 ,[0  for j in  range(i)]]]
 		for m in  range(1,C+1):
-			if p[i-1]<=m and l[i-1]+ ant[m-p[i-1]][0] > ant[m][0]:
-				act.append ([l[i-1]+ ant[m-p[i-1]][0] , ant[m-p[i -1]][1][:]+[1]])
+			if p[i-1]<=m and l[i-1]+ pre[m-p[i-1]][0] > pre[m][0]:
+				pos.append ([l[i-1]+ pre[m-p[i-1]][0] , pre[m-p[i -1]][1][:]+[1]])
 			else:
-				act.append ([ant[m][0],ant[m][1][:]+[0]])
-		ant=act
-	return  act[C]
+				pos.append ([pre[m][0],pre[m][1][:]+[0]])
+		pre=pos
+	return  pos[C]
 
 print(camion_vacas(pesovacas1,litrosleche1,pesocamion1)[0])
 print(camion_vacas(pesovacas2,litrosleche2,pesocamion2)[0])
